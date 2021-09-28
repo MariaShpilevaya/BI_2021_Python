1765558225b17deb72cf30854e486726917703d2
Maria Shpilevaya <moriakoto1998@gmail.com>
2021-09-28 19:45:54 +0500

    First himework editds are here

--- task#1.py
+++ task#1.py
@@ -1,6 +1,10 @@
+rna_seq = ['a', 'c', 'u', 'g']
+dna_seq = ['a', 'c', 't', 'g']
+
+
 def what_to_do(command):
     if command.lower() == 'transcribe':
-        transcribe(input('Enter sequence: '))
+        transcribe(input('Enter DNA sequence: '))
     elif command.lower() == 'exit':
         return 'signal'
     elif command.lower() == 'reverse':
@@ -14,9 +18,9 @@ def what_to_do(command):
 
 
 def transcribe(sequence):
-    if set(sequence.lower()).issubset(['a', 'c', 't', 'g']):
+    if set(sequence.lower()).issubset(dna_seq):
         replacements = {'a': 'u', 'g': 'c', 't': 'a', 'c': 'g'}
-        transcribed_sequence = ''
+        transcribed_sequence = []
         for base in sequence:
             lower_case_base = base.lower()
             if base.islower():
@@ -29,37 +33,44 @@ def transcribe(sequence):
 
 
 def reverse(sequence, do_print=True):
-    if set(sequence.lower()).issubset(['a', 'c', 't', 'g']):
+    if set(sequence.lower()).issubset(['a', 'c', 't', 'g']) or set(sequence.lower()).issubset(['a', 'c', 'u', 'g']):
         reversed_sequence = sequence[::-1]
         if do_print:
             print('Here you go: ', reversed_sequence)
         return reversed_sequence
     else:
-        reverse(input('Only DNA sequence will do, so please try again: '))
+        reverse(input('Only DNA or RNA sequence will do, so please try again: '))
 
 
 def complement(sequence):
-    if set(sequence.lower()).issubset(['a', 'c', 't', 'g']):
-        replacements = {'a': 't', 'g': 'c', 't': 'a', 'c': 'g'}
-        complementary_sequence = ''
+    def complementary_sequence(sequence):
+        resulting_seq = []
         for base in sequence:
             lower_case_base = base.lower()
             if base.islower():
-                complementary_sequence += replacements.get(lower_case_base)
+                resulting_seq += replacements.get(lower_case_base)
             else:
-                complementary_sequence += replacements.get(lower_case_base).upper()
-        print('Here you go: ', complementary_sequence)
+                resulting_seq += replacements.get(lower_case_base).upper()
+        print('Here you go: ', ''.join(resulting_seq))
+
+    if set(sequence.lower()).issubset(rna_seq):
+        replacements = {'a': 't', 'g': 'c', 'u': 'a', 'c': 'g'}
+        complementary_sequence(sequence)
+
+    elif set(sequence.lower()).issubset(dna_seq):
+        replacements = {'a': 'u', 'g': 'c', 't': 'a', 'c': 'g'}
+        complementary_sequence(sequence)
     else:
-        complement(input('Only DNA sequence will do, so please try again: '))
+        complement(input('Only DNA or RNA sequence will do, so please try again: '))
 
 
 def reverse_complement(sequence):
-    if set(sequence.lower()).issubset(['a', 'c', 't', 'g']):
+    if set(sequence.lower()).issubset(dna_seq) or set(sequence.lower()).issubset(rna_seq):
         complement(reverse(sequence, do_print=False))
     else:
-        reverse_complement(input('Only DNA sequence will do, so please try again: '))
+        reverse_complement(input('Only DNA or RNA sequence will do, so please try again: '))
 
-git
+
 while True:
     if what_to_do(input('Enter your command: ').lower()):
         print("Thanks for checking in...see you next time")
